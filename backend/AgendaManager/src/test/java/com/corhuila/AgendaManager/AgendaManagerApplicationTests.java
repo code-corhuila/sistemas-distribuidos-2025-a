dd68a647b49680595fe60899a11ce48ba45af9ba
Jesús Ariel González Bonilla <ariel5253@hotmail.com>
2025-03-05 19:28:40 -0500

    ✨  deevelop: spring boot project structure

---
+++ backend/AgendaManager/src/test/java/com/corhuila/AgendaManager/AgendaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corhuila.AgendaManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
